e44adb0c18144260e9e8abde4d181c66b3d44505
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-09-20 09:43:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const table=document.getElementById("myTable");
 function createTable() {
     //Write your code here
-  
+  let rows=prompt("Input number of rows");
+let columns=prompt("Input number of columns");
+	if(rows==0|| columns==0){
+		alert("able cannot be created");
+	}
+	for(let i=0; i<rows; i++){
+    let start=document.createElement("tr");
+    for(let j=0; j<columns; j++){
+        let data=document.createElement("td");
+		start.append(data);
+	}
+		table.append(start);
+	}
 }
